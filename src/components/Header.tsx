f3d2c0b5b58104d4fb4fd22f769e77befcde5b2c
Samuel <sammyturner.0ye@gmail.com>
2026-04-28 05:32:39 +0100

    fix: add missing useState and useEffect imports to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useWallet } from './WalletProvider';
